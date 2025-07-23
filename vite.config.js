9ff51191a828b62c8c0fae0cba33efc2707fd6c6
esmaakar <esmaakar171@gmail.com>
2025-07-24 01:35:13 +0300

    Açıklama

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Insider-internship-tmdb-frontend-project-phase-1/',
   plugins: [react()],
 })
